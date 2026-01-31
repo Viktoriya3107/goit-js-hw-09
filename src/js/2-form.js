2f45fea46132781ae2a21c5d68ac5304f0af168e
Viktoriya3107 <bondarenko_vikyoriya988@ukr.net>
2026-02-01 01:45:55 +0200

    hw-9

---
+++ src/js/2-form.js
@@ -0,0 +1,51 @@
+
+const STORAGE_KEY = "feedback-form-state";
+
+const formEl = document.querySelector(".feedback-form");
+
+let formData = {
+  email: "",
+  message: "",
+};
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+
+if (savedData) {
+  const parsedData = JSON.parse(savedData);
+
+  formData = {
+    email: parsedData.email || "",
+    message: parsedData.message || "",
+  };
+
+  formEl.elements.email.value = formData.email;
+  formEl.elements.message.value = formData.message;
+}
+
+formEl.addEventListener("input", event => {
+  const { name, value } = event.target;
+
+  if (!name) return;
+
+  formData[name] = value.trim();
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+formEl.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const { email, message } = formData;
+
+  if (!email || !message) {
+    alert("Fill please all fields");
+    return;
+  }
+
+  console.log(formData);
+
+  
+  localStorage.removeItem(STORAGE_KEY);
+  formData = { email: "", message: "" };
+  formEl.reset();
+});
